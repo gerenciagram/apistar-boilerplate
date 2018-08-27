6e62b1efc0b67e7943b0fb4470e56f95f406ba00
Tássio Augusto <tassio.acg@gmail.com>
2018-08-27 18:09:06 -0300

    adding business module structure

--- app/views.py
+++ app/views.py
@@ -5,19 +5,17 @@ from apistar.validators import Integer
 from apistar.http import Host
 
 from app.routes import route
-from app.types import (
-    Account, AccountId
-)
+from app import types
 
 
 #
 # Account
 #
 
-from app.account import business as account_business
+from app.business import account as account_business
 
 
 @route('GET', '/account')
-def get_account() -> Account:
+def get_account() -> types.Account:
     """Get the logged in account."""
     return account_business.get_account(1)
